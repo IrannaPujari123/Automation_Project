9740371263fdd6b220ac5ab12063b0c1b8e34640
IrannaPujari@ <irannapujari14@gmail.com>
2025-12-09 11:13:23 +0530

    First commit in HomePage branch

---
+++ src/test/java/com/TestCases/HomePageTestCase.java
@@ -0,0 +1,22 @@
+package com.TestCases;
+
+import org.testng.annotations.Test;
+
+public class HomePageTestCase {
+
+	@Test
+	void testHomePage()
+	{
+		System.out.println("Home page");
+	}
+	@Test
+	void testHomePage1()
+	{
+		System.out.println("Home page 1");
+	}
+	@Test
+	void testHomePage2()
+	{
+		System.out.println("Home page 2");
+	}
+}
