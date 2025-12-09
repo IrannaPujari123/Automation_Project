6478bfa5818a33cfbfad4da130f10cbd884135c5
IrannaPujari@ <irannapujari14@gmail.com>
2025-12-09 14:54:57 +0530

    creat logout functionality in feature_Logout branch

---
+++ src/test/java/com/TestCases/LogoutTestCases.java
@@ -0,0 +1,22 @@
+package com.TestCases;
+
+import org.testng.annotations.Test;
+
+public class LogoutTestCases {
+
+	@Test
+	void testLogout1()
+	{
+		System.out.println("Logout page 1");
+	}
+	@Test
+	void testLogout2()
+	{
+		System.out.println("Logout page 2");
+	}
+	@Test
+	void testLogout3()
+	{
+		System.out.println("Logout page 3");
+	}
+}
